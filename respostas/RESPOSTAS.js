fe10cd785ec3cd0339fc2d492c16c5ae97266589
produtoskairos <92273536+produtoskairos@users.noreply.github.com>
2021-10-10 20:34:54 -0300

    Add files via upload

--- respostas/RESPOSTAS.js
+++ respostas/RESPOSTAS.js
@@ -6,16 +6,16 @@ exports.preçoss = () => {
 	꙳_Salgados Comuns_ - R$ 75,00
 	   • Coxinha, bolinha de presunto e mussarela, empada\nquibe, croquete com carne ou pimenta biquinho\nrisole de milho ou carne, bolinho de mandioca com carne de sol
 	   
-	꙳_Pastel Comum_ - R$ 68,00
+	꙳ _Pastel Comum_ - R$ 68,00
 	   • Carne, queijo, pizza, frango, margarita
 
-	꙳_Pastel de Angu_ - R$ 68,00
+	꙳ _Pastel de Angu_ - R$ 68,00
 	   • 4 Queijos, frango com catupiry, carne de sol   
 	   
-	꙳_Mini Pizza_ - R$ 140,00
+	꙳ _Mini Pizza_ - R$ 140,00
 	
-	꙳_Mini Hamburguer_ - R$ 160,00
+	꙳ _Mini Hamburguer_ - R$ 160,00
 
-	꙳_Bolinho de mandioca com bacalhau_ - R$ 85,00`
+	꙳ _Bolinho de mandioca com bacalhau_ - R$ 85,00`
 }
 
